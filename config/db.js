e349ce0dcb731a0745f06ea0d35f91eba18bc1d9
vinek maurya <111780715+vinekmaurya2329@users.noreply.github.com>
2023-06-08 08:18:55 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import colors from 'colors';
 
 const connectDB = async ()=>{ 
     try {
-const conn =await mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce');
+const conn =await mongoose.connect('mongodb+srv://vinekmaurya2329:vinek7068594746@cluster0.5iwx2uw.mongodb.net/?retryWrites=true&w=majority');
 console.log(`connected to mongodb database ${conn.connection.host}`)
     }
     catch (error){
